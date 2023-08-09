ea30a9f4188ba1eaa83b965bb7f4ace9052b4c43
Daniel <danielodom23@gmail.com>
2023-08-09 17:36:30 -0400

    Add tokenBurns field

--- src/models/tokenLocks/tokenLocksInfo.js
+++ src/models/tokenLocks/tokenLocksInfo.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
+const tokenBurn = new mongoose.Schema({
+  amount: { type: String, required: true },
+  hash: { type: String, required: true, unique: true },
+});
+
 const tokenLocksInfo = new mongoose.Schema(
   {
     chain: { type: Number, required: true },
     lastScannedBlock: { type: Number, required: true },
+    tokenBurns: [tokenBurn],
   },
   { collection: "tokenLocksInfo" }
 );
--- src/models/vestingLocks/vestingLocksInfo.js
+++ src/models/vestingLocks/vestingLocksInfo.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
+const tokenBurn = new mongoose.Schema({
+  amount: { type: String, required: true },
+  hash: { type: String, required: true, unique: true },
+});
+
 const vestingLocksInfo = new mongoose.Schema(
   {
     chain: { type: Number, required: true },
     lastScannedBlock: { type: Number, required: true },
+    tokenBurns: [tokenBurn],
   },
   { collection: "vestingLocksInfo" }
 );
