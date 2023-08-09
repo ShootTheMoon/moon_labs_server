02d526c6ec755c601b5e72c0fff9ea0e03fb8203
Daniel <danielodom23@gmail.com>
2023-08-09 17:36:45 -0400

    Remove logo found log

--- src/utils/coinGekoHelpers.js
+++ src/utils/coinGekoHelpers.js
@@ -14,8 +14,6 @@ const handleCoinGekoLogos = async (address, chain) => {
       } catch (err) {
         // console.log(err);
       }
-    } else {
-      console.log("exists");
     }
   } catch (err) {
     console.log(err);
